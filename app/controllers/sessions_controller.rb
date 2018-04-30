9244aad5c5960550f634f40218d0e62fce93464d
Randall Harris Haynes <harrishaynes@gmail.com>
2018-04-30 14:40:08 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
   
   def create
-    if user = User.find_by(:name => params["user"]["name"]).try(:authenticate => params["user"]["password"])
+    if user = User.find_by(:name => params["user"]["name"]).try(:authenticate, params["user"]["password"])
       session[:user_id] = user.id
       redirect_to home_path
     else
